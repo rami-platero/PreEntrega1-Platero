c7b96eb99630e899211c7d8546a282abe23d12d9
rami-platero <ramiplatero11@gmail.com>
2023-11-13 21:26:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <div className="bg-zinc-900 min-h-screen w-screen text-white">
+      <header>
+        <NavBar/>
+      </header>
+      <main className="mx-auto max-w-[1400px]">
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
