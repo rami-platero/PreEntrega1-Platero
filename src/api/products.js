57709059e00ae6169b4ea21ac816e653e4377fee
rami-platero <ramiplatero11@gmail.com>
2023-12-06 11:44:02 -0300

    feat: add api functions for fetching of products data

---
+++ src/api/products.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const URL = "../src/data/products.json"
+
+export const getProducts = async () => {
+    const res = await axios.get(URL)
+    return res.data
+}
+
+export const getProductsByCategory = async (category) => {
+    const res = await axios.get(URL)
+    const products = res.data.filter((product)=>{
+        return product.category.id === category
+    })
+    return products
+}
+
+export const getProductById = async (id) => {
+    const res = await axios.get(URL)
+    const product = res.data.find((p)=>{
+        return p.id === Number(id)
+    })
+    return product
+}
