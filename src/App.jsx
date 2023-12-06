8673815930871e3b0e353ba444aab7441ad3ce77
rami-platero <ramiplatero11@gmail.com>
2023-12-06 11:35:17 -0300

    feat: setup and define initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import ItemListContainer from "./components/ItemListContainer"
-import NavBar from "./components/NavBar"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
-    <div className="bg-zinc-900 min-h-screen w-screen text-white">
-      <header>
-        <NavBar/>
-      </header>
-      <main className="mx-auto max-w-[1400px] pt-[100px]">
-        <ItemListContainer greeting={"Welcome to Gaming Components!"}/>
-      </main>
+    <div className="bg-zinc-900 min-h-screen text-white">
+      <BrowserRouter>
+        {/* layout */}
+        <header>
+          <NavBar />
+        </header>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ItemListContainer greeting={"Welcome to Gaming Components!"} />
+            }
+          />
+          <Route
+            path="/category/:category"
+            element={
+              <ItemListContainer greeting={"Welcome to Gaming Components!"} />
+            }
+          />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
