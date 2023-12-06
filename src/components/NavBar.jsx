e89318be58c63b75ab0d860c3d5df254db034c95
rami-platero <ramiplatero11@gmail.com>
2023-12-06 11:42:29 -0300

    refactor: update navigation links to Link components from react-router-dom

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,37 @@
+import { Link } from "react-router-dom";
 import CartWidget from "./CartWidget";
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 w-full bg-black p-4 min-h-[60px]">
+    <nav className="fixed top-0 w-full bg-black p-4 min-h-[60px] z-50">
       <div className="mx-auto w-full max-w-[1400px] flex justify-between items-center">
+        <Link to={"/"}>
+          <img className="max-w-[40px]" src="../public/favicon.png" />
+        </Link>
         <ul className="flex gap-4 text-sm">
           <li>
-            <a
+            <Link
               className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
-              href="/"
+              to={"/category/GPU"}
             >
               Graphics Cards
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
-              href="/"
+              to={"/category/mouse"}
             >
-              Processors
-            </a>
+              Mouses
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
-              href="/"
+              to={"/category/motherboard"}
             >
-              Monitors
-            </a>
+              Motherboards
+            </Link>
           </li>
         </ul>
         <CartWidget />
