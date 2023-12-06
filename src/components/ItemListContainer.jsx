2cacb01f0d66eee89c9b9f6258b7798cea54f355
rami-platero <ramiplatero11@gmail.com>
2023-12-06 11:46:15 -0300

    feat: handle logic to fetch all products and category products

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,33 @@
-const ItemListContainer = ({greeting}) => {
-  return (
-    <section className="flex flex-col items-center">
-        <h1 className="text-2xl">{greeting}</h1>
-    </section>
-  )
-}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProducts, getProductsByCategory } from "../api/products";
+import ProductsList from "./ProductsList";
 
-export default ItemListContainer
+const ItemListContainer = ({ greeting }) => {
+  const [products, setProducts] = useState(null);
+  const { category } = useParams();
+
+  const handleProducts = async () => {
+    const getData = category ? getProductsByCategory : getProducts;
+
+    try {
+      const data = await getData(category);
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleProducts();
+  }, [category]);
+
+  return (
+    <main className="mx-auto max-w-[1400px] pt-[100px] p-4 flex flex-col items-center gap-4">
+      <h1 className="text-2xl">{greeting}</h1>
+      {products ? <ProductsList products={products}/> : null}
+    </main>
+  );
+};
+
+export default ItemListContainer;
