3d6425bc58dd8d6b4090ff6ea774f69facc57cec
rami-platero <ramiplatero11@gmail.com>
2023-12-06 11:43:02 -0300

    feat: add NotFound component

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='pt-[100px] flex items-center justify-center h-full'>
+        <h1 className='font-medium text-2xl'>404 NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default NotFound
