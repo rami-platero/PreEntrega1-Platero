3cf387a9f605a77dd0e136c5fabd7fc2867c93cf
rami-platero <ramiplatero11@gmail.com>
2023-12-11 20:02:09 -0300

    refactor: replace button with Link for more details in product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { formatPrice } from "../lib/utils";
 
 const ProductCard = ({ product }) => {
-  const navigate = useNavigate();
-
   const formattedPrice = formatPrice(product.price)
 
   return (
     <div
-      className="border-solid rounded-lg bg-black bg-opacity-20 border border-zinc-500 p-4 flex flex-col cursor-pointer hover:opacity-90 transition-all ease-in-out duration-200"
-      onClick={() => [navigate(`/item/${product.id}`)]}
+      className="border-solid rounded-lg bg-black bg-opacity-20 border border-zinc-500 p-4 flex flex-col"
     >
       <div className="w-full max-w-xs m-auto object-cover">
         <img
@@ -22,9 +19,9 @@ const ProductCard = ({ product }) => {
       <div className="flex flex-1 flex-col justify-between max-md:items-center">
         <h3 className="font-bold">{product.name}</h3>
         <h4 className="text-sm font-semibold">{formattedPrice}</h4>
-        <button className="bg-indigo-700 bg-opacity-50 border-2 border-indigo-500 w-full py-2 mt-4 rounded-2xl text-sm  text-zinc-200 font-semibold hover:bg-indigo-500 transition ease-in-out">
-          Add to cart
-        </button>
+        <Link to={`/item/${product.id}`} className="bg-indigo-700 bg-opacity-50 border-2 border-indigo-500 w-full py-2 mt-4 rounded-2xl text-sm  text-zinc-200 font-semibold hover:bg-indigo-500 transition ease-in-out text-center">
+          More details
+        </Link>
       </div>
     </div>
   );
