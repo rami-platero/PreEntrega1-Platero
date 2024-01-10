8b1cbc4cfe2d056279ac86367fb0bde1b69ff9bd
rami-platero <ramiplatero11@gmail.com>
2024-01-10 00:11:09 -0300

    setup firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.VITE_REACT_apiKey,
+  authDomain: import.meta.VITE_REACT_authDomain,
+  projectId: import.meta.VITE_REACT_projectId,
+  storageBucket: import.meta.VITE_REACT_storageBucket,
+  messagingSenderId: import.meta.VITE_REACT_messagingSenderId,
+  appId: import.meta.VITE_REACT_appId,
+  measurementId: import.meta.VITE_REACT_measurementId
+};
+
+initializeApp(firebaseConfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
