8a7f36aac1c4f54279dbf1238d0258f6652508f8
rami-platero <ramiplatero11@gmail.com>
2024-01-12 00:34:52 -0300

    feat: add new providers (Toaster, CartContextProvider)

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {initializeApp} from 'firebase/app'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { CartContextProvider } from "./context/CartContext.jsx";
+import { Toaster } from "sonner";
 
-const firebaseConfig = {
-  apiKey: import.meta.VITE_REACT_apiKey,
-  authDomain: import.meta.VITE_REACT_authDomain,
-  projectId: import.meta.VITE_REACT_projectId,
-  storageBucket: import.meta.VITE_REACT_storageBucket,
-  messagingSenderId: import.meta.VITE_REACT_messagingSenderId,
-  appId: import.meta.VITE_REACT_appId,
-  measurementId: import.meta.VITE_REACT_measurementId
-};
-
-initializeApp(firebaseConfig)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Toaster richColors/>
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
+  </React.StrictMode>
+);
