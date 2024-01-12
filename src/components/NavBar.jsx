8c39434896ccf3059863d27226763a4a75da4040
rami-platero <ramiplatero11@gmail.com>
2024-01-12 00:32:48 -0300

    feat: add mobile responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import CartWidget from "./CartWidget";
-import Logo from '../../public/favicon.png'
+import Logo from "../../public/favicon.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="fixed top-0 w-full bg-black p-4 min-h-[60px] z-50">
-      <div className="mx-auto w-full max-w-[1400px] flex justify-between items-center">
+      <div className="mx-auto w-full max-w-[1400px] hidden justify-between items-center sm:flex">
         <Link to={"/"}>
           <img className="max-w-[40px]" src={Logo} />
         </Link>
@@ -21,7 +26,7 @@ const NavBar = () => {
           <li>
             <Link
               className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
-              to={"/category/mouse"}
+              to={"/category/mouses"}
             >
               Mouses
             </Link>
@@ -29,7 +34,7 @@ const NavBar = () => {
           <li>
             <Link
               className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
-              to={"/category/motherboard"}
+              to={"/category/motherboards"}
             >
               Motherboards
             </Link>
@@ -37,6 +42,59 @@ const NavBar = () => {
         </ul>
         <CartWidget />
       </div>
+
+      <button
+        className="sm:hidden"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {!isOpen ? (
+          <RxHamburgerMenu size={"1.5rem"} />
+        ) : (
+          <IoCloseOutline size={"1.5rem"} />
+        )}
+      </button>
+
+      {isOpen ? (
+        <div className="flex flex-col bg-black gap-8 sm:hidden">
+          <ul className="gap-4 text-sm flex flex-col mt-4">
+            <li>
+              <Link
+                className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
+                to={"/"}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
+                to={"/category/GPU"}
+              >
+                Graphics Cards
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
+                to={"/category/mouses"}
+              >
+                Mouses
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-zinc-300 hover:text-white transition duration-200 ease-in-out"
+                to={"/category/motherboards"}
+              >
+                Motherboards
+              </Link>
+            </li>
+          </ul>
+          <CartWidget />
+        </div>
+      ) : null}
     </nav>
   );
 };
