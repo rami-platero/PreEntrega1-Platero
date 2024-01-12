e3539f5dbfbb089fd02f2217f729dd37151e387d
rami-platero <ramiplatero11@gmail.com>
2024-01-12 00:40:27 -0300

    refactor: use full items count from cart context

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { cartContext } from '../context/CartContext'
 
 const CartWidget = () => {
+
+  const {itemsCount} = useContext(cartContext)
+
   return (
-    <div className='flex relative w-fit'>
+    <Link to={"/cart"} className='flex relative w-fit'>
         <AiOutlineShoppingCart size={"1.6rem"}/>
-        <span className='absolute -top-2 -right-2 px-[6px] text-xs bg-red-600 rounded-full'>0</span>
-    </div>
+        <span className='absolute -top-2 -right-2 px-[6px] text-xs bg-red-600 rounded-full'>{itemsCount}</span>
+    </Link>
   )
 }
 
